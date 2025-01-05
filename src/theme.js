964f804465dd29c1790a662d2f03687f1b58b90c
phiewter <19486709+phiewter@users.noreply.github.com>
2025-01-05 23:20:36 +0100

    chore: Add color variables

--- src/theme.js
+++ src/theme.js
@@ -3,19 +3,33 @@ const { getColors } = require("./colors");
 function getTheme({ theme, name }) {
   const color = getColors(theme);
 
+  const border = color.gray[200]; // #1F1F1F
+  const foreground = color.gray[1000]; // #EDEDED
+  const secondaryForeground = color.gray[900]; // #A1A1A1
+  const background = color.background[100]; // #000
+  const secondaryBackground = color.background[200]; // #0A0A0A
+
   return {
     name: name,
     colors: {
-      // Editor
-      "editor.background": color.background[200],
-      "editor.foreground": color.gray[1000],
-      "editor.selectionBackground": "#FFFFFF25",
-      "editor.selectionHighlightBackground": "#FFFFFF25",
-      "editorLineNumber.foreground": color.gray[900],
-      "editorGroupHeader.tabsBackground": "#101010",
+      // Editor colors
+      "editor.background": secondaryBackground,
+      "editor.foreground": foreground,
+
+      "editorLineNumber.foreground": color.gray[600],
+      "editorLineNumber.activeForeground": foreground,
+      "editorLineNumber.dimmedForeground": color.gray[400],
+
+      "editor.selectionBackground": color.blue[700],
+      "editor.selectionHighlightBackground": color.blue[700],
+
+      "editorGroupHeader.tabsBackground": color.background[200],
+
       "editorWidget.background": color.background[200],
+
       "editorWarning.foreground": color.amber[500],
       "editorError.foreground": color.red[500],
+
       "editorOverviewRuler.border": "#101010",
       "editorGutter.addedBackground": "#99FFE4",
       "editorGutter.deletedBackground": "#FF8080",
