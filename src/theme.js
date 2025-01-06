842ff33bfc96b0f967a1c65b2e00f4c1feee0fa3
phiewter <19486709+phiewter@users.noreply.github.com>
2025-01-06 15:31:34 +0100

    fix(theme): Updated 'Editor colors'

--- src/theme.js
+++ src/theme.js
@@ -60,27 +60,23 @@ function getTheme({ theme, name }) {
       "editorLineNumber.foreground": color.gray[600],
       "editorLineNumber.activeForeground": foreground,
       "editorLineNumber.dimmedForeground": color.gray[400],
-
       "editor.selectionBackground": color.blue[700],
       "editor.selectionHighlightBackground": color.blue[700],
-
-      "editorGroupHeader.tabsBackground": color.background[200],
-
-      "editorWidget.background": color.background[200],
-
+      "editorGroupHeader.tabsBackground": secondaryBackground,
+      "editorWidget.background": secondaryBackground,
       "editorWarning.foreground": color.amber[500],
       "editorError.foreground": color.red[500],
 
-      "editorOverviewRuler.border": "#101010",
-      "editorGutter.addedBackground": "#99FFE4",
-      "editorGutter.deletedBackground": "#FF8080",
-      "editorGutter.modifiedBackground": "#FFC799",
-      "diffEditor.insertedTextBackground": "#99FFE415",
-      "diffEditor.insertedLineBackground": "#99FFE415",
-      "diffEditor.removedTextBackground": "#FF808015",
-      "diffEditor.removedLineBackground": "#FF808015",
-      "editorInlayHint.foreground": "#A0A0A0",
-      "editorInlayHint.background": "#1C1C1C",
+      "editorOverviewRuler.border": border,
+      "editorGutter.addedBackground": color.green[600],
+      "editorGutter.deletedBackground": color.red[600],
+      "editorGutter.modifiedBackground": color.amber[600],
+      "diffEditor.insertedTextBackground": color.green[300],
+      "diffEditor.insertedLineBackground": color.green[100],
+      "diffEditor.removedTextBackground": color.red[300],
+      "diffEditor.removedLineBackground": color.red[100],
+      // "editorInlayHint.foreground": color.pink[700],
+      // "editorInlayHint.background": color.pink[700],
     },
   };
 }
