e3cc7a9b0ab53fecc4c30ac054ea0a90612404e4
phiewter <19486709+phiewter@users.noreply.github.com>
2025-01-18 17:41:49 +0100

    test(theme): evaluate contrast colors

--- src/theme.js
+++ src/theme.js
@@ -20,6 +20,10 @@ function getTheme({ theme, name }) {
       focusBorder: color.gray[400],
       "icon.foreground": color.gray[900],
 
+      // Contrast colors
+      contrastActiveBorder: color.gray[1000],
+      contrastBorder: color.gray[1000],
+
       // Button control
       "button.background": color.blue[700],
       "button.foreground": foreground,
